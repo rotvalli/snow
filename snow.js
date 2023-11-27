8030d6b1c45970e4b14e7d8d7d51d374b55a8932
Otto Salminen <otto.salminen@iki.fi>
2023-11-27 20:53:19 +0200

    Canvas snow

--- snow.js
+++ snow.js
@@ -1,92 +1,94 @@
-const styles = `
-    .flake{
-        border-radius: 1px;
-        background: white;
-        position: absolute;
-        z-index: 999999;
-    }
-`;
-const body = document.body, html = document.documentElement;
-const minFps = 20;
-const flakeSize = 4.0;
-const speed = 0.6;
-const density = 2;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-let flakes = [];
-let oldTimeStamp;
-let height;
+let width = window.innerWidth;
+let height = window.innerHeight;
+canvas.height = height;
+canvas.width = width;
 
-const appendStyleSheet = (id, content) => {
-    if (!document.querySelector("#" + id)) {
-        const head = document.head || document.getElementsByTagName("head")[0];
-        head.appendChild(createStyleElement(id, content));
-    }
-}
-
-const createStyleElement = (id, content) => {
-    var style = document.createElement("style");
-    style.id = id;
-
-    if (style.styleSheet) {
-        style.styleSheet.cssText = content;
-    } else {
-        style.appendChild(document.createTextNode(content));
-    }
-    return style;
-}
-
-const updateHeight = () => Math.max(body.scrollHeight, body.offsetHeight,
-    html.clientHeight, html.scrollHeight, html.offsetHeight);
+const count = 4000;
+const drag = 0.25;
+const gravity = 0.68;
+let snowflakes = [];
+let angle = 0;
+let landCount = 0;
+let wave1 = 20;
+let wave2 = 30;
+let phase1 = 100;
+let phase2 = 300;
+let width1 = 100;
+let width2 = 200;
 
 const seedFlake = (top) => {
-    const newFlake = document.createElement("div");
-    newFlake.classList.add("flake");
-    newFlake.style.top = top + "px";
-    newFlake.style.left = (Math.random() * 97.0) + "%";
-    newFlake.style.rotate = Math.round(Math.random() * 360.0) + "deg";
-    newFlake.style.opacity = Math.random() + 0.2;
-    newFlake.style.width = flakeSize + "px";
-    newFlake.style.height = flakeSize + "px";
-    body.appendChild(newFlake);
-}
-
-const remove = (sel) => document.querySelectorAll(sel).forEach(el => el.remove());
-
-const update = () => {
-    flakes.map((flake) => {
-        const eTop = parseFloat(flake.style.top || 0)
-        if (eTop > height - (flakeSize * 2.0)) {
-            flake.style.top = (-1 * flakeSize) + "px";
-        }
-        else {
-            flake.style.top = (eTop + speed + Math.random()) + "px";
-        }
-        flake.style.left = (parseFloat(flake.style.left || 0.0) + ((Math.random() - 0.5) / 3.8)) + "%";
-    });
-}
-
-const loop = (timeStamp) => {
-    const secondsPassed = (timeStamp - oldTimeStamp) / 1000;
-    oldTimeStamp = timeStamp;
-    const fps = Math.round(1 / secondsPassed);
-    if (fps > minFps) {
-        update();
-    }
-    window.requestAnimationFrame(loop);
+    snowflakes.push({
+        x: Math.floor(width / 2 + Math.sin(Math.random() * 360) * width),
+        y: top?0:Math.floor(Math.random() * height),
+        radius: Math.random() * 5 + 1,
+        opacity: Math.floor(Math.random() * 10)/10 ,
+        angle: Math.random(),
+        down: false
+    })
 }
 
 const init = () => {
-    height = updateHeight();
-    remove(".flake");
-
-    Array(density).fill(null).map(
-        _ => Array(height).fill(null).map((_ , h) => seedFlake(h))
-    );
-
-    flakes = [...body.getElementsByClassName("flake")];
+    wave1 = Math.random()*20+10;
+    wave2 = Math.random()*30+10;
+    phase1 = Math.random()*4000;
+    phase2 = Math.random()*2000;
+    width1 = Math.random()*50+100;
+    width2 = Math.random()*150+100;
+    snowflakes = [];
+    landCount = 0;
+    Array(count).fill(null).map(_ => seedFlake(false))
 }
+const draw = () => {
+    snowflakes.map(f => {
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(255,255,255," + f.opacity + ")";
+        ctx.moveTo(f.x, f.y);
+        ctx.arc(f.x, f.y, f.radius, 0, Math.PI * 2, true);
+        ctx.fill();
+    });
+};
 
-appendStyleSheet("snowflakes", styles);
+const move = () => {
+    angle += 0.005;
+    snowflakes.map(f => {
+        if (!f.down) {
+            f.x += Math.sin(drag * angle + f.angle) * f.radius;
+            f.y += f.radius / 2 * gravity;
+            const sinCurve1 = ((wave1-angle)*(Math.sin((f.x-phase1)/width1)));
+            const sinCurve2 = ((wave2+angle)*(Math.sin((f.x-phase2)/width2)));
+            const snowDepth = Math.floor(landCount/(width/8)) + sinCurve1 + sinCurve2 + 10;
+
+            if(snowDepth > canvas.height){
+                landCount = 0;
+                init();             
+            }
+
+            if (f.y > canvas.height - snowDepth) {
+                landCount++;
+                f.down = true;
+                seedFlake(true);
+            }
+        }
+    });
+};
+
+const update = () => {
+    ctx.clearRect(0, 0, width, height);
+    move();
+    draw();
+    window.requestAnimationFrame(update);
+};
+
+const resize = () => {
+    init();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+};
+
 init();
-window.requestAnimationFrame(loop);
-window.onresize = init;
+update();
+window.onresize = resize;
